67994e2441fad698c28bb11d89d84e8bb62eed89
h678282 <sirnesingvild@gmail.com>
2026-04-07 18:10:21 +0200

    finished task 2
    implemented createReplicaFiles() in FileManager.

--- src/main/java/no/hvl/dat110/util/FileManager.java
+++ src/main/java/no/hvl/dat110/util/FileManager.java
@@ -59,12 +59,17 @@ public class FileManager {
 	public void createReplicaFiles() {
 	 	
 		// set a loop where size = numReplicas
-		
-		// replicate by adding the index to filename
-		
-		// hash the replica
-		
-		// store the hash in the replicafiles array.
+		for (int i = 0; i < numReplicas; i++) {
+
+			// replicate by adding the index to filename
+			String replica = filename + i;
+
+			// hash the replica
+			BigInteger fileid = Hash.hashOf(replica);
+
+			// store the hash in the replicafiles array.
+			replicafiles[i] = fileid;
+		}
 	}
 	
     /**
